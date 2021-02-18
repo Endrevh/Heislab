3e9712a455ad5d249a00f170ff46c5ddf0e9eebd
Endre Vee Hagestuen <endre.hagestuen@hotmail.com>
2021-02-18 12:05:18 +0100

    Flere kommentarer

--- skeleton_project/source/management.h
+++ skeleton_project/source/management.h
@@ -19,12 +19,12 @@ void managementDepart(void);
 
 int managementElevatorAtFloor(void); //sjekker om heisen er ved en etasje. 
 
-void managementStopAtFloor(int floor); //sjekker om heisen skal stoppe ved gitt etasje. Må se om noen skal av, eller om noen skal på i den retningen heisen er på vei (bruk state til å sjekke dette)
+void managementStopAtFloor(int floor);  //sjekker om heisen skal stoppe ved gitt etasje. Må se om noen skal av, eller om noen skal på i den retningen heisen er på vei (bruk state til å sjekke dette)
                                         //kalles når vi går forbi en etasje                                       
                                         //bytte state
                                         //må aldri gå lenger ned enn nederste request, og motsatt. Dette kunne skjedd dersom man er i idle,
                                         //får en request lenger nede som skal opp. Da vil heisen begynne å gå nedover selv om det er ingen lenger nede som skal ned eller av.
-                                       
+                                        //foreslått løsning: sjekke om det er noen i etasjene under som skal av eller oppover. Hvis ja, fortsett nedover. Sett opp et eksempel med dette, se hva som fungerer. Kanskje ha en bool variabel som settes til true/false når første idle-request er fullført
 
 
 #endif
